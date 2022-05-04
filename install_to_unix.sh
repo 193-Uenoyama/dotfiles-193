0c4dd0b07190a9d2ff437fa3ec4b988b532549d1
193-Uenoyama <dokkoikitamura193@gmail.com>
2022-05-05 03:56:48 +0900

    add comment

--- install_to_unix.sh
+++ install_to_unix.sh
@@ -6,10 +6,12 @@ ln -s ~/dotfiles-193/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles-193/.bash_aliases ~/.bash_aliases
 echo "source ~/.bash_aliases" >> ~/.bash_profile
 
+# install dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
 ~/installer.sh ~/.cache/dein
 rm ~/installer.sh
 
+# install volta
 curl https://get.volta.sh | bash
 
 # font のインストール
