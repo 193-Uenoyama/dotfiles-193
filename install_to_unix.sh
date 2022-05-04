bc53830d4898932a86c0aa8df4460ae7d4fbc3fc
193-Uenoyama <dokkoikitamura193@gmail.com>
2022-05-05 03:55:07 +0900

    add volta

--- install_to_unix.sh
+++ install_to_unix.sh
@@ -3,12 +3,14 @@
 ln -s ~/dotfiles-193/vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles-193/vim/.gvimrc ~/.gvimrc
 ln -s ~/dotfiles-193/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles-193/.bashrc ~/.bashrc
-echo "source ~/.bashrc" >> ~/.bash_profile
+ln -s ~/dotfiles-193/.bash_aliases ~/.bash_aliases
+echo "source ~/.bash_aliases" >> ~/.bash_profile
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
 ~/installer.sh ~/.cache/dein
 rm ~/installer.sh
+
+curl https://get.volta.sh | bash
 
 # font のインストール
 # git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
